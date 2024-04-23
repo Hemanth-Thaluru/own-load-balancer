ad23175d0994b60f90898f9ad828f94922e97842
Hemanth-Thaluru <thaluruhemanth105@gmail.com>
2024-04-23 17:10:45 -0400

    cleared port bug

--- src/server1.ts
+++ src/server1.ts
@@ -7,7 +7,7 @@ response.writeHead(200,{'content-Type':'text/plain'})
 response.end(' This response is from server-1\n')
 })
 
-// Listen for the request
+// Listen for the request in the designated port
 server1.listen(SERVER1_PORT,()=>{
     console.log(`Server-1 is running at http://localhost:${SERVER1_PORT}\n`)
 })
